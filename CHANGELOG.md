6930b3a9f5f550ddb02e8fa508855da2a67cab99
Semantic Release <semantic-release@rmgmedia.com>
2026-08-10 23:51:28 +0000

    chore(conductor-wordpress-platform-support): [skip ci] Semantic release 4.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,18 @@
+[4.1.0](https://github.com/conductorphp/conductor-wordpress-platform-support/compare/4.0.0...4.1.0) (2026-08-10)
+
+### Features
+* PHP 8.4.1+ (CTAP-1224) ([66f0fbd](https://github.com/conductorphp/conductor-wordpress-platform-support/commit/66f0fbd9c453a9647109c95c24eeb5fa72fa8e54))
+* laminas-servicemanager v4 (CTAP-1221) ([831c78e](https://github.com/conductorphp/conductor-wordpress-platform-support/commit/831c78e8d16e7b88d878ba59583a4d3a7388826f))
+
+<!--- CHANGELOG SPLIT MARKER -->
+
+[3.0.0](https://github.com/conductorphp/conductor-wordpress-platform-support/compare/2.0.0...3.0.0) (2026-06-26)
+
+### Bug Fixes
+* conductor 2.0 (CTAP-776) ([3411cdb](https://github.com/conductorphp/conductor-wordpress-platform-support/commit/3411cdba20328c4dac674951a1d15b695f547e06))
+
+<!--- CHANGELOG SPLIT MARKER -->
+
 [1.0.1](https://github.com/conductorphp/conductor-wordpress-platform-support/compare/1.0.0...1.0.1) (2026-06-25)
 
 ### Bug Fixes
--- src/Maintenance/AppMaintenanceStrategyFactory.php
+++ src/Maintenance/AppMaintenanceStrategyFactory.php
@@ -2,30 +2,16 @@
 
 namespace ConductorWordpressPlatformSupport\Maintenance;
 
-use Interop\Container\ContainerInterface;
-use Interop\Container\Exception\ContainerException;
-use Laminas\ServiceManager\Exception\ServiceNotCreatedException;
-use Laminas\ServiceManager\Exception\ServiceNotFoundException;
 use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
 
 class AppMaintenanceStrategyFactory implements FactoryInterface
 {
-
-    /**
-     * Create an object
-     *
-     * @param  ContainerInterface $container
-     * @param  string             $requestedName
-     * @param  null|array         $options
-     *
-     * @return object
-     * @throws ServiceNotFoundException if unable to resolve the service.
-     * @throws ServiceNotCreatedException if an exception is raised when
-     *     creating a service.
-     * @throws ContainerException if any other error occurs
-     */
-    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
-    {
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        ?array $options = null
+    ): AppMaintenanceStrategy {
         $applicationConfig = $container->get('ConductorAppOrchestration\Config\ApplicationConfig');
         return new AppMaintenanceStrategy($applicationConfig);
     }
